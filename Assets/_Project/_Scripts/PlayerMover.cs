4260567cb74088bd598b62658fdb66793fa46901
Maciej Góralczyk <goralczykmaciek@gmail.com>
2025-03-21 00:48:46 +0100

    Added player control.

--- Assets/_Project/_Scripts/PlayerMover.cs
+++ Assets/_Project/_Scripts/PlayerMover.cs
@@ -4,16 +4,37 @@ namespace WildernessExplorer
 {
     public class PlayerMover : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        [SerializeField] float speed = 5f;     
+        [SerializeField] float turnSpeed = 3f; 
+        Rigidbody _rb;
+
         void Start()
         {
-        
+            _rb = GetComponent<Rigidbody>();
+            _rb.freezeRotation = true;
         }
 
-        // Update is called once per frame
-        void Update()
+        void FixedUpdate()
         {
-        
+            Vector3 moveDirection = Vector3.zero;
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                moveDirection += transform.forward;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(Vector3.up, -turnSpeed);
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(Vector3.up, turnSpeed);
+            }
+
+            var move = moveDirection * (speed * Time.fixedDeltaTime); 
+            _rb.MovePosition(move + _rb.position);
         }
     }
 }
